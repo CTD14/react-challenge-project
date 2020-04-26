da87e3357ab34f588dd168209a691dc01b90b59e
george hofstetter <georgehofstetter@georges-iMac.local>
2020-04-26 12:10:26 -0700

    Just updating a change in nav.js, and vieworders.js

--- application/src/components/view-orders/viewOrders.js
+++ application/src/components/view-orders/viewOrders.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Template } from '../../components';
+import { Link } from "react-router-dom";
 import { SERVER_IP } from '../../private';
 import './viewOrders.css';
 
@@ -19,6 +20,23 @@ class ViewOrders extends Component {
                 }
             });
     }
+    if 
+
+    deleteAnOrder(order) {
+        fetch(`${SERVER_IP}/api/delete-order`, {
+            method: 'POST',
+            body: JSON.stringify({
+                id: order._id
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(res => res.json())
+        .then(response => console.log(response.success))
+        .catch(error => console.error(error));
+    }
+
 
     render() {
         return (
@@ -33,12 +51,14 @@ class ViewOrders extends Component {
                                     <p>Ordered by: {order.ordered_by || ''}</p>
                                 </div>
                                 <div className="col-md-4 d-flex view-order-middle-col">
-                                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}:${createdDate.getSeconds()}`}</p>
+                                    <p>Order placed at {`${createdDate.getHours()}:${createdDate.getMinutes()}`}</p>
                                     <p>Quantity: {order.quantity}</p>
                                  </div>
                                  <div className="col-md-4 view-order-right-col">
+                                 <Link to={"/order"} className="nav-link">
                                      <button className="btn btn-success">Edit</button>
-                                     <button className="btn btn-danger">Delete</button>
+                                     </Link>
+                                     <button className="btn btn-danger" onClick={(event) => this.deleteAnOrder(order)}>Delete</button>
                                  </div>
                             </div>
                         );
